dd7e56d5b7acc81afcd5edf594702a89e7a6bcad
Shawn Yeo <khyeo151@gmail.com>
2017-06-22 13:40:55 +1200

    media noti repo addition

---
+++ Omac.Data/Infrastructure/Repositories/MediaRepository.cs
@@ -0,0 +1,19 @@
+﻿using Omack.Data.DAL;
+using Omack.Data.Models;
+
+namespace Omack.Data.Infrastructure.Repositories
+{
+	public class MediaRepository : GenericRepository<Media>, IMediaRepository
+	{
+		public MediaRepository(OmackContext context) : base(context)
+		{
+
+		}
+	}
+
+
+	public interface IMediaRepository : IGenericRepository<Media>
+	{
+
+	}
+}
